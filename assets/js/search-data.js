d57b32ede10223f66453f85fcd6e610b4ba6ecca
indri-a <140671967+indri-a@users.noreply.github.com>
2025-09-13 23:19:30 +0000

    Deploying to gh-pages from @ indri-a/indri-a.github.io@131e8963848bcddb259217ab92faf4acf369f32e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,7 @@ ninja.data = [{
               window.location.href = "/projects/climate_hack/";
             },},{id: "projects-slam-dunk-contest-scoring-analysis",
           title: 'Slam Dunk Contest Scoring Analysis',
-          description: "Statistics class extra-credit project",
+          description: "Analyzing the new NBA Slam Dunk Contest scoring format",
           section: "Projects",handler: () => {
               window.location.href = "/projects/slam_dunk_contest/";
             },},{
